e9ab5c3b9016ff9402b19f7e5210adb8d4ca76a9
Evgenii <vorobeievgeniy@gmail.com>
2024-07-13 00:14:47 +0300

    done

--- src/main/kotlin/de/maibornwolff/its/buildergenerator/util/IdeaExtensions.kt
+++ src/main/kotlin/de/maibornwolff/its/buildergenerator/util/IdeaExtensions.kt
@@ -2,18 +2,8 @@ package de.maibornwolff.its.buildergenerator.util
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.psi.PsiDirectory
-import kotlin.contracts.ExperimentalContracts
-import kotlin.contracts.contract
 import org.jetbrains.kotlin.psi.KtClass
 
 fun AnActionEvent.getClassUnderCaret() = this.dataContext.getData("psi.Element") as? KtClass
-
-@ExperimentalContracts
-fun KtClass?.isNonNullDataClass(): Boolean {
-    contract {
-        returns(true) implies (this@isNonNullDataClass != null)
-    }
-    return this != null && this.isData()
-}
 
 fun KtClass.getContainingDirectory(): PsiDirectory = this.containingFile.containingDirectory
